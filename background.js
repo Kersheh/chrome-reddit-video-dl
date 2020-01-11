43f0fb2f1198c209d10e0e76001d69082845c4fa
Matt Breckon <mattbreckon@gmail.com>
2020-01-10 22:11:01 -0500

    Context menu only visible on video right click

--- background.js
+++ background.js
@@ -1,12 +1,22 @@
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    chrome.tabs.sendMessage(tabs[0].id, { downloadVideo: true });
+chrome.runtime.onMessage.addListener(request => {
+  chrome.contextMenus.removeAll(() => {
+    if(request.event === 'createContextMenuItem') {
+      if(!chrome.contextMenus.onClicked.hasListeners()) {
+        chrome.contextMenus.onClicked.addListener((info, tab) => openVideoNewTab(info, tab, request.videoId));
+      }
+
+      chrome.contextMenus.create({
+        title: 'Download reddit video',
+        id: 'parent'
+      });
+    }
   });
 });
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    title: 'Download reddit video',
-    id: 'parent'
+function openVideoNewTab(info, tab, videoId) {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      downloadVideo: true, videoId: videoId
+    });
   });
-});
+}
--- reddit-video-dl.js
+++ reddit-video-dl.js
@@ -1,34 +1,45 @@
-chrome.runtime.onMessage.addListener((request, sender) => {
-  if(request.downloadVideo) {
-    const videos = document.querySelectorAll('[id^=media-preview]');
+const videos = document.querySelectorAll('[id^=media-preview]');
 
-    videos.forEach(video => {
-      const fileName = `${video.id}.webm`;
-      const videoId = video.id.replace('media-preview-', '');
+document.addEventListener('mousedown', () => {
+  chrome.runtime.sendMessage({ event: 'removeContextMenuItem' });
+});
 
-      const videoRoot = document.getElementById(`video-${videoId}`);
-      const dataMpdUrl = videoRoot.getAttribute('data-mpd-url');
-
-      fetch(dataMpdUrl, {
-        'Content-Type': 'text/xml; charset=utf-8',
-        'Accept-Encoding': 'gzip, deflate'
-      })
-        .then(response => response.text())
-        .then(str => (new window.DOMParser()).parseFromString(str, 'text/xml'))
-        .then(data => {
-          const dataMpdPlaylist = JSON.parse(xml2json(data, ''));
-
-          // TODO: retrieve largest video src; naively assumes first option is largest video
-          const baseURL = dataMpdPlaylist.MPD.Period.AdaptationSet[0] ?
-            dataMpdPlaylist.MPD.Period.AdaptationSet[0].Representation[0].BaseURL :
-            dataMpdPlaylist.MPD.Period.AdaptationSet.Representation[0].BaseURL;
-          const videoUrl = `${dataMpdUrl.replace('/DASHPlaylist.mpd', '')}/${baseURL}`;
-          
-          // TODO: engage download instead of opening video in new tab (which allows right-click save on video so solution works for now)
-          window.open(videoUrl, '_blank').focus();
-        })
-        .catch(console.error);
+videos.forEach(video => {
+  video.addEventListener('mousedown', event => {
+    chrome.runtime.sendMessage({ 
+      event: 'createContextMenuItem',
+      videoId: video.id
     });
+  }, true);
+});
+
+chrome.runtime.onMessage.addListener(request => {
+  if(request.downloadVideo && request.videoId) {
+    // const fileName = `${request.videoId}.webm`;
+    const videoId = request.videoId.replace('media-preview-', '');
+
+    const videoRoot = document.getElementById(`video-${videoId}`);
+    const dataMpdUrl = videoRoot.getAttribute('data-mpd-url');
+
+    fetch(dataMpdUrl, {
+      'Content-Type': 'text/xml; charset=utf-8',
+      'Accept-Encoding': 'gzip, deflate'
+    })
+      .then(response => response.text())
+      .then(str => (new window.DOMParser()).parseFromString(str, 'text/xml'))
+      .then(data => {
+        const dataMpdPlaylist = JSON.parse(xml2json(data, ''));
+
+        // TODO: retrieve largest video src; naively assumes first option is largest video
+        const baseURL = dataMpdPlaylist.MPD.Period.AdaptationSet[0] ?
+          dataMpdPlaylist.MPD.Period.AdaptationSet[0].Representation[0].BaseURL :
+          dataMpdPlaylist.MPD.Period.AdaptationSet.Representation[0].BaseURL;
+        const videoUrl = `${dataMpdUrl.replace('/DASHPlaylist.mpd', '')}/${baseURL}`;
+        
+        // TODO: engage download instead of opening video in new tab (which allows right-click save on video so solution works for now)
+        window.open(videoUrl, '_blank').focus();
+      })
+      .catch(console.error);
   }
 });
 
